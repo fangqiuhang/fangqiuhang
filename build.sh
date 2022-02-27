fcaf8d28cafa742a43463aded70f9594a563b997
fangqiuhang <fangqiuhang@163.com>
2022-02-27 18:18:05 +0800

    Add RFC9001 sass and fix bugs

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 ./scripts/sass.py
 hugo
+find public -name "*.scss" | xargs rm
--- scripts/sass.py
+++ scripts/sass.py
@@ -10,7 +10,8 @@ SASS_TASKS = (
         "DES_PATH": "content/RFC8999_Chinese_Translation/css/RFC8999.css"},
     {"SRC_PATH": "content/RFC9000_Chinese_Translation/sass/RFC9000.scss",
         "DES_PATH": "content/RFC9000_Chinese_Translation/css/RFC9000.css"},
-    #{"SRC_PATH": "sass/fangqiuhang.scss", "DES_PATH": "static/css/fangqiuhang.css"},
+    {"SRC_PATH": "content/RFC9001_Chinese_Translation/sass/RFC9001.scss",
+        "DES_PATH": "content/RFC9001_Chinese_Translation/css/RFC9001.css"},
 )
 
 if __name__ == "__main__":
